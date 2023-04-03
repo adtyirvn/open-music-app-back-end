e7f54c276891df9c82255cde216990d4974fb042
Aditya Irvan <adityairvanku@gmail.com>
2023-04-03 12:40:31 +0700

    add validation and fix bugs

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -2,12 +2,15 @@
 const autoBind = require('auto-bind');
 
 class AlbumsHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
     autoBind(this);
   }
 
   async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+
     const { name, year } = request.payload;
     const albumId = await this._service.addAlbum({ name, year });
 
